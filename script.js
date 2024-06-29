99e82423041cef522ad74025fb97cd49af7cb000
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2024-06-29 13:14:28 +0530

    Links are working

---
+++ script.js
@@ -0,0 +1,21 @@
+const githubLink = document.querySelector(".github-Link");
+const instaLink = document.querySelector(".insta-Link");
+const XLink = document.querySelector(".X-Link");
+const linkedinLink = document.querySelector(".linkedin-Link");
+
+githubLink.addEventListener("click",() => { 
+    window.open("https://github.com/SonuuChowdhury/")
+ });
+
+instaLink.addEventListener("click",() => { 
+    window.open("https://www.instagram.com/sonuu.chowdhury/")
+ });
+
+XLink.addEventListener("click",() => { 
+    window.open("https://x.com/SonuuChowdhury")
+ });
+
+linkedinLink.addEventListener("click",() => { 
+    window.open("https://www.linkedin.com/in/sonu-chowdhury-5378612b2/")
+ });
+;
